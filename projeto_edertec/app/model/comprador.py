e0b54674135072b0732bd86c63fe4f536a28c101
Adm-Trattoria <projetotrattoria@gmail.com>
2024-08-26 15:00:27 -0300

    unido

---
+++ projeto_edertec/app/model/comprador.py
@@ -0,0 +1,11 @@
+class Comprador:
+
+    def __init__(self,id_c,cpf,nome,email,senha,numero):
+        self.id_c = id_c
+        self.nome = nome
+        self.cpf = cpf
+        self. email = email
+        self. senha= senha
+        self. numero = numero
+
+    
---
+++ projeto_edertec/app/model/ingresso.py
@@ -0,0 +1,9 @@
+class Ingresso:
+
+    def __init__(self,id_ing,tipo,nome,restricao):
+        self.id_ing = id_ing
+        self.tipo = tipo
+        self.nome = nome
+        self. restricao = restricao
+
+    
